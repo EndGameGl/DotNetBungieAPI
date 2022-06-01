6d65ac30b3850ae54d0492353964017a2d64ff5e
MeGl <neloangelo3105@gmail.com>
2022-06-01 11:50:23 +0300

    Fix to how code is exchanged

--- DotNetBungieAPI.AspNet.Security.OAuth.Providers/BungieNetAuthenticationHandler.cs
+++ DotNetBungieAPI.AspNet.Security.OAuth.Providers/BungieNetAuthenticationHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Net;
 using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Text.Encodings.Web;
@@ -46,23 +47,71 @@ public partial class BungieNetAuthenticationHandler : OAuthHandler<BungieNetAuth
             await Log.UserProfileErrorAsync(Logger, response, Context.RequestAborted);
             throw new HttpRequestException("An error occurred while retrieving the user profile.");
         }
-        
+
         using var payload = JsonDocument.Parse(await response.Content.ReadAsStringAsync(Context.RequestAborted));
-        
+
         var principal = new ClaimsPrincipal(identity);
-        var context = new OAuthCreatingTicketContext(principal, properties, Context, Scheme, Options, Backchannel, tokens, payload.RootElement);
+        var context = new OAuthCreatingTicketContext(principal, properties, Context, Scheme, Options, Backchannel,
+            tokens, payload.RootElement);
         context.RunClaimActions();
-        
+
         await Events.CreatingTicket(context);
         return new AuthenticationTicket(context.Principal!, context.Properties, Scheme.Name);
     }
 
+    protected override async Task<OAuthTokenResponse> ExchangeCodeAsync(OAuthCodeExchangeContext context)
+    {
+        var tokenRequestParameters = new List<KeyValuePair<string, string>>()
+        {
+            new("grant_type", "authorization_code"),
+            new("code", context.Code),
+            new("client_id", Options.ClientId),
+            new("client_secret", Options.ClientSecret)
+        };
+
+        if (context.Properties.Items.TryGetValue(OAuthConstants.CodeVerifierKey, out var codeVerifier))
+        {
+            tokenRequestParameters.Add(new(OAuthConstants.CodeVerifierKey, codeVerifier!));
+            context.Properties.Items.Remove(OAuthConstants.CodeVerifierKey);
+        }
+
+        using var request = new HttpRequestMessage(HttpMethod.Post, Options.TokenEndpoint);
+
+        request.Content = new FormUrlEncodedContent(tokenRequestParameters);
+
+        using var response = await Backchannel.SendAsync(request, Context.RequestAborted);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            await Log.ExchangeCodeErrorAsync(Logger, response, Context.RequestAborted);
+            return OAuthTokenResponse.Failed(new Exception("An error occurred while retrieving an access token."));
+        }
+
+        var payload = JsonDocument.Parse(await response.Content.ReadAsStringAsync(Context.RequestAborted));
+
+        return OAuthTokenResponse.Success(payload);
+    }
+
     private static partial class Log
     {
-        internal static async Task UserProfileErrorAsync(ILogger logger, HttpResponseMessage response,
+        internal static async Task UserProfileErrorAsync(
+            ILogger logger,
+            HttpResponseMessage response,
             CancellationToken cancellationToken)
         {
             UserProfileError(
+                logger,
+                response.StatusCode,
+                response.Headers.ToString(),
+                await response.Content.ReadAsStringAsync(cancellationToken));
+        }
+
+        internal static async Task ExchangeCodeErrorAsync(
+            ILogger logger,
+            HttpResponseMessage response,
+            CancellationToken cancellationToken)
+        {
+            ExchangeCodeError(
                 logger,
                 response.StatusCode,
                 response.Headers.ToString(),
@@ -76,6 +125,16 @@ public partial class BungieNetAuthenticationHandler : OAuthHandler<BungieNetAuth
         private static partial void UserProfileError(
             ILogger logger,
             System.Net.HttpStatusCode status,
+            string headers,
+            string body);
+
+        [LoggerMessage(
+            2,
+            LogLevel.Error,
+            "An error occurred while retrieving an access token: the remote server returned a {Status} response with the following payload: {Headers} {Body}.")]
+        private static partial void ExchangeCodeError(
+            ILogger logger,
+            HttpStatusCode status,
             string headers,
             string body);
     }
